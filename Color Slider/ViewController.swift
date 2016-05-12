e65ff6e2305aca6558f4f92a714f54e71f4de167
Fahad Al Rabbani <alfahad1993@gmail.com>
2016-05-12 02:22:30 +0100

    Initial Commit
    First commit of the app to Github.

--- Color Slider/ViewController.swift
+++ Color Slider/ViewController.swift
@@ -10,14 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    @IBOutlet weak var square: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        changeColour()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func changeColour() {
+    
+        let rFloat = redSlider.value
+        let r : CGFloat = CGFloat(rFloat)
+        let gFloat = greenSlider.value
+        let g : CGFloat = CGFloat(gFloat)
+        let bFloat = blueSlider.value
+        let b : CGFloat = CGFloat(bFloat)
+        
+        square.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
+        
+        
     }
 
 
